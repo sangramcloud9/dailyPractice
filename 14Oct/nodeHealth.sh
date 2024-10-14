79037df97bdd30c104625e706b739b71624dff09
Ubuntu <ubuntu@ip-172-31-37-243.ap-south-1.compute.internal>
2024-10-14 09:09:35 +0000

    git status

--- 14Oct/nodeHealth.sh
+++ 14Oct/nodeHealth.sh
@@ -8,12 +8,12 @@ set -e #this will exit the script if there is an error
 
 set -o pipefail #this will overcome the drawback of set e
 
-free -g #this will print the RAM usage
+free -g >nodeHealthReport #this will print the RAM usage
 
-df -h #this will print the disk usage
+df -h >>nodeHealthReport #this will print the disk usage
 
-ps -ef #this will print all the running processes
+ps -ef >> nodeHealthReport #this will print all the running processes
 
-ps -ef | grep amazon #filter amazon
+ps -ef | grep amazon >> nodeHealthReport  #filter amazon
 
-ps -ef | grep amazon | awk -F" " '{print $2}' #filter column 2
+ps -ef | grep amazon | awk -F" " '{print $2}' >> nodeHealthReport #filter column 2
